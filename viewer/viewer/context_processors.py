691a9bac4e61124bbf7cd8d6c8e3778a7bfd8c6d
Andy Chosak <andy.chosak@cfpb.gov>
2022-07-20 15:15:12 -0400

    Move crawl stats context to new WARC page model

--- viewer/viewer/context_processors.py
+++ viewer/viewer/context_processors.py
@@ -3,14 +3,14 @@ import os
 from django.conf import settings
 from django.db.models import Count, Max, Min
 
-from .models import Page
+from warc.models import Page
 
 
 def crawl_stats(request):
-    crawl_stats = Page.objects.values("crawled_at").aggregate(
-        count=Count("crawled_at"),
-        start=Min("crawled_at"),
-        end=Max("crawled_at"),
+    crawl_stats = Page.objects.values("timestamp").aggregate(
+        count=Count("timestamp"),
+        start=Min("timestamp"),
+        end=Max("timestamp"),
     )
 
     crawl_stats.update(
