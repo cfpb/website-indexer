cfpb/website-indexer

commit fbc343f360aeb54eda2f93aaa29977102b94302b

Wget: ignore most query string parameters
When using wget to crawl the website, skip all query string parameters
except for "page", "ext_url", and "signature".

We need "page" to make sure we go through e.g. the entire blog. We need
"ext_url" and "signature" so that we visit external link pages so we
can properly determine when we actually link off of the website.

This commit includes a new utility Python script for extracting all of
the query string parameters used on the website, so that we know what
to tell wget to ignore. This runs on a pages.csv file generated using
"read_warc.py dump_csvs".

wget_crawl.sh
@@ -4,7 +4,7 @@
 #
 # Example usage:
 #
-# ./crawl.sh [-d depth] https://www.consumerfinance.gov/
+# ./wget_crawl.sh [-d depth] https://www.consumerfinance.gov/
 #
 # Optionally specify -d depth to limit the crawl depth.
 
@@ -71,12 +71,13 @@ time wget \
     --delete-after \
     --no-directories \
     --warc-file=crawl \
-    --warc-cdx \
+    --warc-cdx=on \
     --warc-tempdir="$tmp_dir" \
     --execute robots=off \
     --limit-rate=200k \
     --ignore-case \
     --reject '*.css,*.csv,*.do,*.doc,*.docx,*.epub,*.gif,*.ico,*.jpg,*.js,*.json,*.mp3,*.pdf,*.png,*.pptx,*.py,*.r,*.sas,*.sps,*.svg,*.tmp,*.txt,*.wav,*.webmanifest,*.woff,*.woff2,*.xls,*xlsx,*.xml,*.zip' \
+    --reject-regex "CatID=|NavCode=|_gl=|activity_type=|authors=|book=|categories=|chartType=|charttype=|clhx=|dateInterval=|date_received_min=|dateinterval=|entx=|fdx=|filter1_topics=|filter2_topics=|form-id=|gib=|gpl=|grade_level=|has_narrative=|hltx=|hous=|houx=|insi=|insl=|inst=|iped=|issue=|language=|lens=|mta=|oid=|othg=|othr=|othx=|page=|parl=|pelg=|perl=|pid=|ppl=|product=|prvf=|prvi=|prvl=|q=|regs=|retx=|schg=|school_subject=|searchField=|search_field=|searchfield=|size=|sort=|stag=|subl=|tab=|taxx=|title=|topic=|topics=|totl=|tran=|trnx=|tuit=|unsl=|utm_campaign=|utm_medium=|utm_source=|wkst=" \
     --recursive \
     --level="$depth" \
     --rejected-log=rejected.log \
@@ -87,7 +88,7 @@ popd > /dev/null
 
 # Copy back logs and WARC file from temporary directory.
 cp "$tmp_dir"/{wget,rejected}.log .
-cp "$tmp_dir"/crawl.warc.gz .
+cp "$tmp_dir"/crawl.{warc.gz,cdx} .
 
 # Clean up temporary directory.
 rm -rf "$tmp_dir"

list_qs_params.py
@@ -1,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Given a pages.csv file generated by "read_warc.py dump_csvs", output a list
+# of query string parameters used across all page URLs. Useful for creating a
+# proper --reject-regex parameter for wget, for example:
+#
+# ./list_qs_params.py pages.csv \
+#     --ignore-param=page \
+#     --ignore-param=ext_url \
+#     --ignore-param=signature \
+#     --wget-reject-regex
+#
+# This command will dump out a list of query string parameters, excluding
+# "page", "ext_url", and "signature" in a format compatible with
+# wget --reject-regex:
+#
+# CatID=|NavCode=|_gl=|activity_type=|...
+#
+# See relevant wget documentation at:
+#
+# https://www.gnu.org/software/wget/manual/html_node/Recursive-Accept_002fReject-Options.html
+import argparse
+import csv
+from itertools import chain
+from operator import itemgetter
+from urllib.parse import parse_qs, urlparse
+
+
+def list_qs_params(pages_csv, ignore_param, wget_reject_regex):
+    reader = csv.reader(pages_csv)
+    urls = list(map(itemgetter(1), reader))
+    params = sorted(set(chain(*(parse_qs(urlparse(url).query).keys() for url in urls))))
+    params = [p for p in params if p not in ignore_param]
+
+    if wget_reject_regex:
+        print("|".join(f"{param}=" for param in params))
+    else:
+        print(params)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("pages_csv", type=argparse.FileType("r", encoding="utf-8"))
+    parser.add_argument("--ignore-param", nargs="*", default=[])
+    parser.add_argument("--wget-reject-regex", action="store_true")
+
+    args = parser.parse_args()
+
+    list_qs_params(**vars(args))

commit 0514e90024525c9b9713c2a209691532f66fa7cf

Restore ?page= parameter to list of crawled pages
Commit fbc343f360aeb54eda2f93aaa29977102b94302b inadvertently excluded
the ?page= parameter from the list of valid query string parameters
when crawling the site with wget. This commit restores it.

wget_crawl.sh
@@ -78,7 +78,7 @@ time wget \
     --random-wait \
     --ignore-case \
     --reject '*.css,*.csv,*.do,*.doc,*.docx,*.epub,*.gif,*.ico,*.jpg,*.js,*.json,*.mp3,*.pdf,*.png,*.pptx,*.py,*.r,*.sas,*.sps,*.svg,*.tmp,*.txt,*.wav,*.webmanifest,*.woff,*.woff2,*.xls,*xlsx,*.xml,*.zip' \
-    --reject-regex "CatID=|NavCode=|_gl=|activity_type=|authors=|book=|categories=|chartType=|charttype=|clhx=|dateInterval=|date_received_min=|dateinterval=|entx=|fdx=|filter1_topics=|filter2_topics=|form-id=|gib=|gpl=|grade_level=|has_narrative=|hltx=|hous=|houx=|insi=|insl=|inst=|iped=|issue=|language=|lens=|mta=|oid=|othg=|othr=|othx=|page=|parl=|pelg=|perl=|pid=|ppl=|product=|prvf=|prvi=|prvl=|q=|regs=|retx=|schg=|school_subject=|searchField=|search_field=|searchfield=|size=|sort=|stag=|subl=|tab=|taxx=|title=|topic=|topics=|totl=|tran=|trnx=|tuit=|unsl=|utm_campaign=|utm_medium=|utm_source=|wkst=" \
+    --reject-regex "CatID=|NavCode=|_gl=|activity_type=|authors=|book=|categories=|chartType=|charttype=|clhx=|dateInterval=|date_received_min=|dateinterval=|entx=|fdx=|filter1_topics=|filter2_topics=|form-id=|gib=|gpl=|grade_level=|has_narrative=|hltx=|hous=|houx=|insi=|insl=|inst=|iped=|issue=|language=|lens=|mta=|oid=|othg=|othr=|othx=|parl=|pelg=|perl=|pid=|ppl=|product=|prvf=|prvi=|prvl=|q=|regs=|retx=|schg=|school_subject=|searchField=|search_field=|searchfield=|size=|sort=|stag=|subl=|tab=|taxx=|title=|topic=|topics=|totl=|tran=|trnx=|tuit=|unsl=|utm_campaign=|utm_medium=|utm_source=|wkst=" \
     --recursive \
     --level="$depth" \
     --rejected-log=rejected.log \