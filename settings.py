a7dbe3a416552c47bf8b465331b8346fd6447a0b
Andy Chosak <andy.chosak@cfpb.gov>
2023-10-30 11:58:19 -0400

    Add timestamp to crawler logging

--- settings.py
+++ settings.py
@@ -140,9 +140,16 @@ REST_FRAMEWORK = {
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": " %(asctime)s.%(msecs)03d %(levelname)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "default",
         },
     },
     "loggers": {
